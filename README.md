c99830e4b04abacb4daba178fed3e0965e88ba2a
Leonardo <leonardostlima@gmail.com>
2022-07-24 23:09:17 -0300

    added eslint and write readme

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# pokedex
+<h1 align="center">
+  Pokedex <img src="./img/.github/pokebol.png" width="50" alt="Pokedex" align="center"> 
+</h1>
+
+<p align="center">
+  <img src="https://img.shields.io/github/repo-size/leonarclo/pokedex">
+  <img src="https://img.shields.io/github/last-commit/leonarclo/pokedex">
+  <img src="https://img.shields.io/github/license/leonarclo/pokedex">
+</p>
+
+_<p align="center">* Status: Concluído ✅ </p>_
+
+---
+
+<p align="center">
+ <a href="#sobre-o-projeto">Sobre</a> •
+ <a href="#demonstração">Demonstração</a> • 
+ <a href="#tecnologias-utilizadas">Tecnologias</a> • 
+ <a href="#funcionalidades">Funcionalidades</a> • 
+  <a href="#dependências">Dependências</a> • 
+ <a href="#como-executar-o-projeto">Como executar</a> • 
+ <a href="#licença">Licença</a> 
+</p>
+
+---
+
+## 📑 Sobre o projeto
+Pokedex, no mundo da franquia de Pokemon, é um dispositivo que mapeia as informações sobre todos os pokemons existentes. Desse modo, este projeto consiste em uma página web de uma pokedex personalizada e adaptada para a demonstração desses pokemons.
+Este projeto tem como base o consumo de uma API Restful, chamada [PokeAPI](https://pokeapi.co/), a qual conta com mais de 900 pokemóns e suas evoluções. Desta API, foram consumidas informações como nome, id, status, tipo, além dos gifs animados de cada Pokemon.
+Esta página web teve como principais desafios: consumo de API com async/await; conceitos/efeitos DOM; responsividade com view height/view width e posicionamento de elementos; JS assíncrono.
+Como referência de código, foi utilizado o projeto desenvolvido [Manual do Dev](https://github.com/manualdodev/pokedex).
+
+---
+
+## 🎨 Demonstração
+<div align="center">
+  <img src="./img/.github/pokedex-demo-gif.gif" alt="Demonstração do layout Pokedex">
+</div>
+
+---
+
+## 🚀 Tecnologias utilizadas
+As seguintes ferramentas foram utilizadas no desenvolvimento do projeto:
+- [**HTML**](https://html.com/)
+- [**CSS**](https://www.w3.org/Style/CSS/)
+- [**Javascript**](javascript.com)
+- [**NodeJS**](https://nodejs.org/en/)
+- [**Visual Studio Code**](https://code.visualstudio.com/)
+
+---
+
+## 📌 Funcionalidades
+- [x] Fetch API
+- [x] Eventos DOM
+- [x] Filtro de pesquisa por Nome e ID
+
+
+---
+
+## 🧲 Dependências
+- [x] **[ESLint](https://eslint.org/)**
+
+---
+
+## 🛠️ Como executar o projeto
+
+#### Pré-requisitos
+
+Além de um <u>navegador de internet atualizado</u>, você precisa ter instalado em sua máquina as seguintes ferramentas: [Git](https://git-scm.com), [Node.js](https://nodejs.org/en/) e um editor de código como [VSCode](https://code.visualstudio.com/). Então, siga as instruções abaixo:
+``` bash
+# Clone este repositório:
+$ git clone https://github.com/leonarclo/pokedex.git
+
+# Entre no diretório:
+$ cd pokedex
+
+# Visualizaçao:
+$ npx liver-server
+
+```
+---
+
+## 📝 Licença
+Este projeto está licenciado sob a licença MIT. Para mais detalhes, acesse o arquivo [LICENSE.md](https://github.com/leonarclo/pokedex/blob/main/LICENSE).
--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const pokemonID = document.querySelector('.pokemon-id');
 const pokemonName = document.querySelector('.pokemon-name');
 const pokemonImg = document.querySelector('.pokemon-img');
+const pokemonImgBg = document.querySelector('.img-bg');
 
 const pokemonDesc = document.querySelector('.pokemon-desc');
 const pokemonType = document.querySelector('.type');
-const pokemonWeight = document.querySelector('.weight');
 const pokemonHP = document.querySelector('.hp');
 const pokemonAttack = document.querySelector('.attack');
 const pokemonDefense = document.querySelector('.defense');
 const pokemonSpeed = document.querySelector('.speed');
+const pokemonWeight = document.querySelector('.weight');
+const pokemonHeight = document.querySelector('.height');
 
 const btnPrev = document.querySelector('.btn-prev');
 const btnNext = document.querySelector('.btn-next');
@@ -16,23 +18,35 @@ const btnNext = document.querySelector('.btn-next');
 const form = document.querySelector('.form-search');
 const inputSearch = document.querySelector('.input-search');
 
-
-
 let searchPokemon = 1;
 
+const pokemonBgColors = {
+  fire: 'radial-gradient(circle at center, #ffffff , #ecafaa)',
+  grass: 'radial-gradient(circle at center, #ffffff , #a7ecab)',
+  electric: 'radial-gradient(circle at center, #ffffff , #Ffd7ae)',
+  water: 'radial-gradient(circle at center, #ffffff , #aEF3FD)',
+  ground: 'radial-gradient(circle at center, #ffffff , #F4E7ee)',
+  rock: 'radial-gradient(circle at center, #ffffff , #d6d5d4)',
+  fairy: 'radial-gradient(circle at center, #ffffff , #FCEAFF)',
+  poison: 'radial-gradient(circle at center, #ffffff , #98D7A5)',
+  bug: 'radial-gradient(circle at center, #ffffff , #F8D5A3)',
+  dragon: 'radial-gradient(circle at center, #ffffff , #97B3E6)',
+  psychic: 'radial-gradient(circle at center, #ffffff , #EAEDA1)',
+  flying: 'radial-gradient(circle at center, #ffffff , #efda78)',
+  fighting: 'radial-gradient(circle at center, #ffffff , #e0f5f5)',
+  normal: 'radial-gradient(circle at center, #ffffff , rgb(145, 145, 145))',
+};
+
 const fecthPokemon = async (pokemon) => {
-
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
   if (APIResponse.status === 200) {
     const responseJSON = await APIResponse.json();
-    return responseJSON; 
+    return responseJSON;
   }
-}
+};
 
 const renderPokemon = async (pokemon) => {
-
-  pokemonName.innerText = 'Loading...'
+  pokemonName.innerText = 'Loading...';
 
   const data = await fecthPokemon(pokemon);
 
@@ -41,37 +55,39 @@ const renderPokemon = async (pokemon) => {
     pokemonDesc.style.display = 'block';
     pokemonID.innerHTML = `${data.id} - `;
     pokemonName.innerHTML = data.name;
-    pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    const type = data.types.map((type) => {
-        return type.type.name
-    })
-    pokemonType.innerHTML = `Tipo: ${type.toString().replace(',', ' | ')}`;
+    pokemonImg.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+    const type = data.types.map((types) => types.type.name);
+    pokemonType.innerHTML = `Tipo: <strong>${type.join(' | ')}</strong>`;
 
     pokemonHP.innerHTML = `HP: ${data.stats[0].base_stat}`;
-    pokemonHP.style.width = `${data.stats[0].base_stat}%`
     pokemonAttack.innerHTML = `Ataque: ${data.stats[1].base_stat}`;
-    pokemonAttack.style.width = `${data.stats[1].base_stat}%`
     pokemonDefense.innerHTML = `Defesa: ${data.stats[2].base_stat}`;
-    pokemonDefense.style.width = `${data.stats[2].base_stat}%`
     pokemonSpeed.innerHTML = `Velocidade: ${data.stats[3].base_stat}`;
-    pokemonSpeed.style.width = `${data.stats[3].base_stat}%`    
-    pokemonWeight.innerHTML = `Peso: ${data.stats[4].base_stat} Kg`;
-  
+    pokemonWeight.innerHTML = `Peso: ${data.weight} kg`;
+    pokemonHeight.innerHTML = `Altura: ${data.height / 10} m`;
+
+    pokemonHP.style.width = `${data.stats[0].base_stat}%`;
+    pokemonAttack.style.width = `${data.stats[1].base_stat}%`;
+    pokemonDefense.style.width = `${data.stats[2].base_stat}%`;
+    pokemonSpeed.style.width = `${data.stats[3].base_stat}%`;
+
+    const color = pokemonBgColors[type[0]];
+    pokemonImgBg.style.background = color;
+
     inputSearch.value = '';
     searchPokemon = data.id;
   } else {
     pokemonID.innerHTML = '';
     pokemonName.innerHTML = 'Not found :c';
-    pokemonImg.style.display = 'none'
-    pokemonDesc.style.display = 'none'
+    pokemonImg.style.display = 'none';
+    pokemonDesc.style.display = 'none';
   }
-}
+};
 
 form.addEventListener('submit', (event) => {
-  
   event.preventDefault();
   renderPokemon(inputSearch.value.toLowerCase());
-})
+});
 
 renderPokemon(searchPokemon);
 
@@ -80,12 +96,12 @@ const prevPokemon = () => {
     searchPokemon -= 1;
     renderPokemon(searchPokemon);
   }
-}
+};
 
 const nextPokemon = () => {
   searchPokemon += 1;
   renderPokemon(searchPokemon);
-}
+};
 
 btnPrev.addEventListener('click', prevPokemon);
 btnNext.addEventListener('click', nextPokemon);
